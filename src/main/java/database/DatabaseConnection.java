e1b4d1afc3090e9c3752de2cee5ba02c2b2728bf
JacobsenKopcietruski <jakub.kopc@gmail.com>
2024-05-13 17:22:03 +0200

    third

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -1,8 +1,6 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseConnection {
     private static Connection connection;
@@ -13,11 +11,51 @@ public class DatabaseConnection {
 
     public static void connect(String path){
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite" + path);
+            connection = DriverManager.getConnection("jdbc:sqlite:" + path);
             System.out.println("Works!");
         }
         catch(SQLException e){
             throw new RuntimeException(e);
         }
     }
+
+    public static void disconnect(){
+        try {
+            if (connection != null && !connection.isClosed()){
+                connection.close();
+                System.out.println("Połączenie zostało poprawnie zamknęte!!!");
+            }
+        }
+        catch(SQLException e){
+
+
+        }
+    }
+
+    public static void insertTrack(int id, String artist, String name, int duration){
+        try {
+            Statement insertStatement = connection.createStatement();
+            StringBuilder sb = new StringBuilder();
+            sb.append("INSERT INTO song ");
+            sb.append("Values ");
+            sb.append(String.format("(%d,'%s','%s','%d')",id,artist,name,duration));
+            insertStatement.executeUpdate(sb.toString());
+        }catch (SQLException e){
+        }
+    }
+
+    public static void readTrack(){
+        try  {
+            Statement selectionStatement = connection.createStatement();
+            String query = "SELECT artist FROM song WHERE id=20; ";
+            ResultSet rs =  selectionStatement.executeQuery(query);
+            while (rs.next()){
+                System.out.println("Artist of song number 20 is " + rs.getString("artist"));
+            }
+        }
+        catch (SQLException e){
+
+        }
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,19 @@
 package org.example;
 
 import database.DatabaseConnection;
+import database.Song;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         DatabaseConnection.connect("songs.db");
+
+        Song song = new Song(49,"Krzysztof Krawczyk","Parostatek", 1000);
+
+
+
+        DatabaseConnection.insertTrack(48,"Zenek","Spadające gwiazdy", 256);
+        DatabaseConnection.disconnect();
     }
 }
